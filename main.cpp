c8c284624fd62dcc710db908f03bf285a2060164
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2022-12-02 07:34:43 -0500

    stuff?

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <SFML/Graphics.hpp>
 #include "particle.cpp"
 #include <iostream>
+#include <stdlib.h> 
+#include <time.h>
 
+
+
+void randomParticles(std::vector<Particle*> *b, int n, sf::Vector2i screen) {
+    for (int i = 0; i < n; i++) {
+        int mass = (int)rand()%1000+1;
+        sf::Color color(rand() % 255, rand() % 255, rand() % 255);
+
+        //Particle *particle = new Particle(800, 600, 10, 10, 0, 0, sf::Color::Red);
+        Particle *particle = new Particle((int)rand()%screen.x, (int)rand()%screen.y, mass, 10, rand()%2-1, rand()%2-1, color);
+        b->push_back(particle);
+    }
+}
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Devoided");
-    window.setFramerateLimit(60);
-    Particle sun(400, 300, 500, 10, 0, 0, sf::Color::Yellow);
-    Particle blue(300, 300, 0, 5, 0, 1, sf::Color::Blue);
-    Particle red(500, 400, 0, 5, 1, 0, sf::Color::Red);
-    std::vector<Particle> bodies = {blue, red, sun};
+    srand(time(NULL));
+    float zoom = 1.0f;
+    bool moving;
+    sf::Vector2f oldPos;
+    sf::RenderWindow window(sf::VideoMode(1980, 1024), "Devoided");
+    window.setFramerateLimit(360);
+    // Particle sun(890, 512, 1000, 10, 0, 0, sf::Color::Yellow);
+    // Particle blue(790, 512, 10, 10,0, 1, sf::Color::Blue);
+    // Particle red(990, 600, 10, 10, 0, -1, sf::Color::Red);
+    // std::vector<Particle*> bodies = {&blue, &red, &sun};
+    std::vector<Particle*> bodies;
+    randomParticles(&bodies, 200, sf::Vector2i(1980, 1024));
     while (window.isOpen())
     {
         sf::Event event;
@@ -21,16 +41,64 @@ int main()
                 sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
                 window.setView(sf::View(visibleArea));
             }
+            if(event.type == sf::Event::MouseWheelScrolled) {
+                if(event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+                    sf::View view = window.getView();
+                    if (event.mouseWheelScroll.delta <= -1)
+                        zoom = std::min(2.f, zoom + .5f);
+                    else if (event.mouseWheelScroll.delta >= 1)
+                        zoom = std::max(.5f, zoom - .5f);
+                    view.zoom(zoom);
+                    window.setView(view);
+                }
+  
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                // Mouse button is pressed, get the position and set moving as active
+                    if (event.mouseButton.button == 0) {
+                        moving = true;
+                        oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+                    }
+                }
+                if (event.type == sf::Event::MouseButtonReleased) {
+                    if (event.mouseButton.button == 0) {
+                        moving = false;
+                    }
+                }
+                if (event.type == sf::Event::MouseMoved)   {
+                  
+                        // Ignore mouse movement unless a button is pressed (see above)
+                        if (!moving) break;
+                        // Determine the new position in world coordinates
+                        const sf::Vector2f newPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+                        // Determine how the cursor has moved
+                        // Swap these to invert the movement direction
+                        const sf::Vector2f deltaPos = oldPos - newPos;
+                        sf::View view = window.getView();
+                        // Move our view accordingly and update the window
+                        view.setCenter(view.getCenter() + deltaPos);
+                        window.setView(view);
+
+                        // Save the new position as the old one
+                        // We're recalculating this, since we've changed the view
+                        oldPos = window.mapPixelToCoords(sf::Vector2i(event.mouseMove.x, event.mouseMove.y));
+                    }
+
         }
 
         window.clear();
-        red.doPhysicsStuff(bodies);
-        sun.doPhysicsStuff(bodies);
-        blue.doPhysicsStuff(bodies);
-        sun.render(window);
-        //std::cout << sun.getPos().x << " " << sun.getPos().y << std::endl;  
-        red.render(window);
-        blue.render(window);
+        //sun.render(window);
+        //red.render(window);
+        //blue.render(window);
+        //red.doPhysicsStuff(bodies);
+        //sun.doPhysicsStuff(bodies);
+        //blue.doPhysicsStuff(bodies);
+       // std::cout << "aa: " << (&bodies[1])->getPos().x << "\n";
+        for (Particle* p : bodies) {
+            //std::cout << "aa: " << p->getPos().x << "\n";
+            p->render(window);
+            p->doPhysicsStuff(bodies);
+        };
         window.display();
     }
 
--- particle.cpp
+++ particle.cpp
@@ -1,22 +1,21 @@
 #include <SFML/Graphics.hpp>
 #include <math.h>
 #include <algorithm>
-
+#include <iostream>
+const double G = 6.67e-1;
 class Particle {
 	sf::Vector2f pos;
     float mass; 
     float radius; 
-    float x, y; 
-    float vX, vY; 
-    float aX, aY;
+    float vX, vY;
     sf::Color color;
     sf::CircleShape s;
 
 
     public:
-    	  Particle(float pos_x, float pos_y, float mass, float radius, float velX, float velY, sf::Color color) {
-        pos.x = pos_x;
-        pos.y = pos_y;
+    	Particle(float pos_x, float pos_y, float mass, float radius, float velX, float velY, sf::Color color) {
+        this->pos.x = pos_x;
+        this->pos.y = pos_y;
         this->mass = mass;
         this->vX = velX;
         this->vY = velY;
@@ -28,7 +27,7 @@ class Particle {
     }
     void render(sf::RenderWindow& w)
     {
-    	s.setPosition(pos);
+        s.setPosition(pos);
         w.draw(s);
     }
 
@@ -40,10 +39,6 @@ class Particle {
     {
         return sf::Vector2f(vX, vY);
     }
-    sf::Vector2f getAccel()
-    {
-        return sf::Vector2f(aX, aY);
-    }
     float getMass()
     {
         return mass;
@@ -52,29 +47,67 @@ class Particle {
     {
         return radius;
     }
-    void doPhysicsStuff(std::vector<Particle> particles) {
-    	for (Particle p : particles) {
-    		if (this != &p) {
-	    		float dX = p.getPos().x - pos.x;
-	    		float dY = p.getPos().y - pos.y;
-	    		float d = sqrt(pow(dX, 2) + pow(dY, 2));
-	    		float id = 1.0f / d;
-	    		float isd = id * id;
-	    		float nX = id * dX;
-	    		float nY = id * dY;
-	    		float acX = nX * p.getMass() * isd;
-	    		float acY = nY * p.getMass() * isd;
-	    		if ((p.getRadius() + this->getRadius()) < d) {
-		    		this->vX += acX;
-		    		this->vY += acY;
-		    		this->pos.x += vX;
-		    		this->pos.y += vY;
-	    		} 
+    void setVel(float x, float y) {
+        vX = x;
+        vY = y;
+    }
+    bool collision(Particle* p, Particle t, sf::Vector2f nPos) {
+        float dX = p->getPos().x - (t.getPos().x + nPos.x + t.getVel().x);
+        float dY = p->getPos().y - (t.getPos().y + nPos.y + t.getVel().y);
+        float d = sqrt((dX * dX) + (dY * dY));
+        return ((p->getRadius() + t.getRadius()) <= d);
+    }
+    void doPhysicsStuff(std::vector<Particle*> particles) {
+    	for (Particle* p : particles) {
+    		if (this != p) {
+                float Fnet;
+                float theta;
+                float accel; 
+	    		float dX = p->getPos().x - pos.x;
+	    		float dY = p->getPos().y - pos.y;
+	    		float d = sqrt((dX * dX) + (dY * dY));
+
+                //Determine the net gravitational force between the two 
+                Fnet = ((G * (getMass() * p->getMass())) / (d * d)); 
+                //Determine the angle from a to b in 2-Dimensional space 
+                theta = atan2(dY, dX); 
+                //Determine the total acceleration 
+                //std::cout << getMass() << " " << p->getMass() << "\n";
+                accel = Fnet / getMass(); 
+                //Find acceleration from vector components 
+                float acX = accel * cos(theta); 
+                float acY = accel * sin(theta);
+	    		// float id = 1.0f / d;
+	    		// float isd = id * id;
+	    		// float nX = id * dX;
+	    		// float nY = id * dY;
+	    		// float acX = nX * p.getMass() * isd;
+	    		// float acY = nY * p.getMass() * isd;float dX = p.getPos().x - pos.x;
+
+                    
+                if (collision(p, *this, sf::Vector2f(acX, acY))) {
+	    		//if ((dX * dX) + (dY * dY) >= (p.getRadius() + getRadius()) ) {
+                    vX += acX;
+                    vY += acY;
+                    //std::cout << pos.x << "\n";
+                    pos.x += vX;
+                    pos.y += vY;
+	    		 } 
+                 else {
+                    vX-=acX;
+                    vY-=acY;
+                    p->setVel(vX, vY);
+                    pos.x += vX;
+                    pos.y += vY;
+                }
+
+                    vX = vY = 0; //TEST
+                    acX = acY = 0;
     		}
     	}
     }
     void doPhysicsStuff(Particle p) {
-		if (p.getPos() != this->pos && p.getVel() != this->getVel()) {
+		if (p.getPos() != pos && p.getVel() != getVel()) {
 			float dX = p.getPos().x - pos.x;
 			float dY = p.getPos().y - pos.y;
 			float d = sqrt(pow(dX, 2) + pow(dY, 2));
