957a8d190f04eee10c5cca0491dde9d3e12ae77f
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-07-10 19:48:04 -0500

    Update App.jsx

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -41,10 +41,6 @@ let universe = null;
 // }
 let planetColors = ["#264653", "2A9D8F", "#F4A261", "E76F51", "#277da1", "#43aa8b", "#E9C46A", "#f8961e", "f94144"]
 function SolarSystem(props) {
-  useEffect(() => {
-    let planet = new Planet(new Vec2(Math.random()*props.size[0], Math.random()*props.size[1]), new Vec2(Math.random()-0.5, Math.random()-0.5), 15, 12, new Color(planetColors[Math.floor(planetColors.length * Math.random())]).toHex())
-    console.log(planet.get_data())
-  }, [])
   const [render, setRender] = useState(0);
   useTick(delta => {
     //console.log("UPDATE")
@@ -54,7 +50,7 @@ function SolarSystem(props) {
   if (universe == null ) {
     if (universe==null) universe = new Universe()
     if (props.random) {
-      //universe.planets.push(new Planet([props.size[0]/2, props.size[1]/2], [0, 0], 15, 10000000, new Color("black")))
+      universe.add_planet(new Planet(new Vec2(props.size[0]/2, props.size[1]/2), new Vec2(0, 0), 15, 10000000, new Color("black").toHex()).get_data())
       for (let i = 0; i < props.randomCount; i++) {
         universe.add_planet(new Planet(new Vec2(Math.random()*props.size[0], Math.random()*props.size[1]), new Vec2(Math.random()-0.5, Math.random()-0.5), 15, 12, new Color(planetColors[Math.floor(planetColors.length * Math.random())]).toHex()).get_data())
       }
