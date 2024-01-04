566bec3c988e64385ac16f00a3add11a5f06344e
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-01-04 01:07:15 -0500

    dev

--- src/tsx/objects/Credits.tsx
+++ src/tsx/objects/Credits.tsx
@@ -5,7 +5,7 @@ export default function Credits() {
     <div className="absolute flex justify-center align-middle bottom-0 left-1/2 -translate-x-1/2 text-neutral-200 bg-black bg-opacity-80">
       <Link
         className="h-min max-h-fit m-2 hover:underline"
-        to="https://notaroomba.xyz"
+        to="https://notaroomba.dev"
       >
         Website
       </Link>
